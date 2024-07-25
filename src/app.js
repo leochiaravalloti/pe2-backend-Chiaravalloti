1f77179782c975ddc7b842e6104bd761d413902c
LeoChiara <leonardochiaravalloti@gmail.com>
2024-07-25 11:21:20 -0300

    former pre-e added

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import handlebars from 'express-handlebars'
+import __dirname from './utils.js'
+import viewsRouter from './routes/views.router.js'
+import cartRouter from "./routes/carrito.router.js"
+import productsRouter from "./routes/products.router.js"
+import { Server } from 'socket.io'
+
+const app = express()
+const PORT = 8080
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.engine('handlebars', handlebars.engine())
+app.set('views', __dirname + '/views')
+app.set('view engine', 'handlebars')
+app.use(express.static(__dirname + '/public'))
+
+
+app.use("/", cartRouter)
+app.use("/", productsRouter)
+
+app.use('/', viewsRouter)
+const httpServer = app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+const socketServer = new Server(httpServer)
